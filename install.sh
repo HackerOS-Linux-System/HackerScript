90194bfdd4d501cabe8a54aa0f75c83ace9db9a4
michal92299 <chudybyczek2137@gmail.com>
2026-01-06 12:15:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -fsSL https://github.com/HackerOS-Linux-System/HackerScript/releases/download/v0.1/HackerScript-Installer \
-&& chmod +x HackerScript-Installer \
+&& chmod a+x HackerScript-Installer \
 && ./HackerScript-Installer \
 && rm -f HackerScript-Installer
