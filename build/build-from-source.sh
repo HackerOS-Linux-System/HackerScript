c47c06ff725f0cda3b7467a9943c6e81b13ecdab
michal92299 <chudybyczek2137@gmail.com>
2026-01-11 17:43:59 +0100

    Update build-from-source.sh

--- build/build-from-source.sh
+++ build/build-from-source.sh
@@ -1,1 +1,20 @@
-
+#!/bin/bash
+echo "Starting building from source"
+cd source-code
+cd star
+python3.13 -m venv venv
+source venv/bin/activate
+pip install python-hsl2
+cargo build --release 
+decativate
+sudo cp -r /target/release/star ../source-code
+cd ..
+cd virus
+../source-code/star main.hcs
+sudo mv cache/build/target/release/virus ../source-code
+cd ..
+cd hspm
+../source-code/star main.hcs
+sudo mv cache/build/target/release/hspm ../source-code
+cd ..
+echo "Build from source complete"
