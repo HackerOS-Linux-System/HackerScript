2ca07630319d2d991aa0e5d79252e93dacc41ea3
michal92299 <chudybyczek2137@gmail.com>
2026-01-17 18:25:16 +0100

    Update parser.rs

--- source-code/HS1/src/parser.rs
+++ source-code/HS1/src/parser.rs
@@ -1,5 +1,3 @@
-use pest::Parser;
-
 #[derive(pest_derive::Parser)]
-#[grammar = "hackerscript.pest"] // ten sam plik .pest co w HS3
+#[grammar = "hackerscript.pest"]
 pub struct HackerScriptParser;
