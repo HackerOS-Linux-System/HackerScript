f8af09383fe94aa146ad5f4a74a4aff46c41397a
michal92299 <chudybyczek2137@gmail.com>
2026-08-28 09:52:46 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+set -e
+
+URL="https://github.com/HackerOS-Linux-System/HackerScript/releases/download/v0.1/virus"
+TARGET_DIR="/usr/bin"
+TEMP_FILE=$(mktemp)
+
+echo "Pobieranie pliku..."
+curl -sSL "$URL" -o "$TEMP_FILE"
+
+echo "Nadawanie uprawnień do wykonywania..."
+chmod +x "$TEMP_FILE"
+
+echo "Instalacja w $TARGET_DIR/..."
+sudo mv "$TEMP_FILE" "$TARGET_DIR/virus"
+
+echo "Instalacja zakończona pomyślnie."
